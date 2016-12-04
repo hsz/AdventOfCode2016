c35b2c0af269876a755d3e5a58ebb779db076ed1
hsz <jakub@chrzanowski.info>
2016-12-04 14:31:26 +0100

    sort util method

--- day03/index.js
+++ day03/index.js
@@ -1,12 +1,12 @@
 import _ from 'lodash';
-import { input, test } from '../utils';
+import { input, test, sort } from '../utils';
 
 const data = file => input(file).split('\n').map(v => _(v.trim().split(/ +/).map(Number)).value());
 
 module.exports = () => {
   const fn = file => zip => {
     const input = zip ? _.chunk([].concat(..._.zip(...data(file))), 3) : data(file);
-    return _.cloneDeep(input).map(a => a.sort((a, b) => a - b)).filter(([a, b, c]) => (a + b > c)).length;
+    return sort(input, (a, b) => a - b).filter(([a, b, c]) => (a + b > c)).length;
   };
 
   test(fn('test')(), 2);
--- utils.js
+++ utils.js
@@ -1,6 +1,9 @@
 import fs from 'fs';
+import _ from 'lodash';
 export { equal as test } from 'assert';
 
 export const input = (filename = 'input') => {
   return fs.readFileSync(`${filename}.txt`, 'utf8').trim();
 };
+
+export const sort = (data, fn) => _.cloneDeep(data).map(a => a.sort(fn));
